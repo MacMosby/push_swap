0bf038b74975f55be7303f9c55a601772dc0b65a
Marc Rodenbusch <marcrodenbusch@gmail.com>
2023-10-19 00:53:49 +0200

    worked out main algorithm to solve problem

---
+++ main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrodenbu <mrodenbu@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 00:45:04 by mrodenbu          #+#    #+#             */
+/*   Updated: 2023/10/19 00:45:08 by mrodenbu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	algorithm()
+{
+	// if 5 or more numbers in stack a
+		// push two numbers from stack a to stack b and sort them the right way
+		// while more than 3 numbers in stack a
+			// reset indexes
+			// looping over stack a
+				// find number w/ minumum number of moves to put in right position
+			// move number in right position
+			// check if still more than 3 numbers on stack a
+}
