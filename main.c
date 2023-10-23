fc5229fcd62058a6e7986f719f2754f4bab15c93
Marc Rodenbusch <marcrodenbusch@gmail.com>
2023-10-23 18:17:09 +0200

    implented first steps to build algorithm

--- main.c
+++ main.c
@@ -13,11 +13,23 @@
 void	algorithm()
 {
 	// if 5 or more numbers in stack a
-		// push two numbers from stack a to stack b and sort them the right way
+	if (num_of_nums(stack_a) > 4)
+		// push two numbers from stack a to stack b and sort them the right way (bigger one on top)
+		push_x_to_y(stack_a, stack_b);
+		push_x_to_y(stack_a, stack_b);
+		if (stack_b->x < stack_b->next->x)
+			swap(stack_b);
 		// while more than 3 numbers in stack a
+		while num_of_nums(stack_a > 3)
 			// reset indexes
 			// looping over stack a
 				// find number w/ minumum number of moves to put in right position
+
+					// find num of steps to bring element to top of stack a
+					// find num of steps to arrange stack b
+					// plus one move (push a to b)
 			// move number in right position
-			// check if still more than 3 numbers on stack a
+		// sort three numbers on stack a
+		// get all numbers from stack b to stack a
+
 }
