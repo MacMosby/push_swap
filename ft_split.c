98a8d86785cc636796798ac35ef25fceebcd3744
Marc Rodenbusch <marcrodenbusch@gmail.com>
2023-11-16 14:53:22 +0100

    completed ft_split, still need to free

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,60 @@
 /*   By: marcrodenbusch <marcrodenbusch@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 12:01:08 by mrodenbu          #+#    #+#             */
-/*   Updated: 2023/11/16 12:49:41 by marcrodenbu      ###   ########.fr       */
+/*   Updated: 2023/11/16 14:52:25 by marcrodenbu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+char	**ft_create_outer_ptr(int wc)
+{
+	char	**ptr;
+
+	ptr = (char **)malloc((wc + 1) * sizeof(char *));
+	if (!ptr)
+		exit(-3);
+	return (ptr);
+}
+
+int ft_get_word_count(char *s)
+{
+	int	i;
+	int	words;
+
+	words = 0;
+	i = 0;
+	if (s[i] == 0)
+		return (0);
+	if (s[i] == ' ')
+		i++;
+		while (s[i] != 0)
+		{
+			if (s[i] == ' ' && s[i - 1] != ' ')
+				words++;
+			i++;
+		}
+		if (s[i] == 0 && s[i - 1] != ' ')
+			words++;
+		return (words);
+}
+
+char	*ft_set_str(int i, int start, char *s)
+{
+	int		j;
+	char	*str;
+
+	str = malloc((i - start + 1) *sizeof(char));
+	if (!str)
+		return (NULL);
+	j = 0;
+	while (j < i - start)
+	{
+		str[j] = s[start + j];
+		j++;
+	}
+	str[j] = 0;
+	return (str);
+}
 
 char	**ft_split(char *str)
 {
@@ -20,28 +69,20 @@ char	**ft_split(char *str)
 	int		start;
 	int		word;
 
-	// find number of words
 	wc = ft_get_word_count(str);
-	// create outer pointer
-	optr = (char **)malloc((wc + 1) * sizeof(char*));
-	if (!optr)
-		exit(-3);
+	optr = ft_create_outer_ptr(wc);
 	i = 0;
 	start = 0;
 	word = 0;
-	// iterate over string
 	while (str[i] && wc > 0)
 	{
-		// if space
 		if (str[i] == ' ' && str[i - 1] != ' ' && i > 0)
-			// create inner pointer and asign
 			optr[word++] = ft_set_str(i, start, str);
-			// copy word to pointer
 		if (str[i] != ' ' && str[i - 1] == ' ' && i > 0)
 			start = i;
 		i++;
 	}
-	if (str[i] == 0 && s[i - 1] != ' ' && wc > 0)
+	if (str[i] == 0 && str[i - 1] != ' ' && wc > 0)
 		optr[word] = ft_set_str(i, start, str);
 	optr[wc] = NULL;
 	return (optr);
